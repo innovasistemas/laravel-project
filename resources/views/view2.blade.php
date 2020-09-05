7b276a51fd175a1c04140a9c369e5f8057b6de21
innovasistemas <innovasistemas@gmailcom>
2020-09-05 04:59:21 -0500

    Laravel: rutas, introducción vistas, assets

---
+++ resources/views/view2.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>View 1</title>
+        <link rel="stylesheet" href="css/styles.css" />
+    </head>
+    <body>
+        <span id="span1" class="color-text1">
+        <?php
+            echo "Hello, this is the View 1 in PHP";
+        ?>
+        </span>
+        
+        
+        <script src="js/jquery-3.4.1.min.js"></script>
+        <script>
+            $(function(){
+                $('#span1').addClass('size-text1');
+            });
+        </script>
+    </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('about-us', function(){
+    return "First route in Laravel";
+});
+
+
+Route::get('view1', function(){
+    return view('view1');
+});
+
+
+Route::get('view2', function(){
+    return view('view2');
+});
+
+
+Route::get('/data/param1/{id}', function($id){
+    return "Parameter entered: {$id}";
+});
+
+
+Route::get('/name/{name}/id/{id}/nickname/{nickname?}', function($name, $id, $nickname=''){
+    if($nickname){
+        return "Name: {$name} -- Id: {$id} -- Nickname: {$nickname}";
+    }else{
+        return "Name: {$name} -- Id: {$id} -- No nickname";
+    }
+})->where(['id' => '[\d]+'])->where(['nickname' => '1|2|3']);
+
+
+Route::get('crud/{option}', function ($slug) { 
+    return "Option: $slug"; 
+})->where(['option' => 'create|delete|update']);
+
+
+
+Route::post('route-post', function(){
+    return "Route post";
+});
